3477519dfbcaacb23013e59235b0efde8f348a53
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-26 21:57:51 +0100

    remove: useless constant

--- src/constants.ts
+++ src/constants.ts
@@ -6,8 +6,6 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRATION_TIME_ACCESS = process.env.JWT_EXPIRATION_TIME_ACCESS;
 export const JWT_EXPIRATION_TIME_REFRESH = process.env.JWT_EXPIRATION_TIME_REFRESH;
 
-export const BCRYPT_SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS;
-
 export const URI_MS_USERS = process.env.URI_MS_USERS;
 
 export const BROKER_URL = process.env.BROKER_URL ?? 'amqp://localhost';
